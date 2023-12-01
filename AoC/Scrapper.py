ab721997578ea530008614e70fa28e991f002ce7
Minorzar <gauthier_biehler@yahoo.com>
2023-12-01 14:44:09 +0100

    Day 1, puzzle 2

--- AoC/Scrapper.py
+++ AoC/Scrapper.py
@@ -2,6 +2,7 @@ import os
 
 cwd = os.getcwd()
 inputFile = r"Input\input_file.txt"
+num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 inputPath = os.path.join(cwd, inputFile)
 
@@ -12,12 +13,18 @@ def counter():
 
     for line in lines:
         numbers = []
+        for n in num:
+            index = line.find(n)
+            while index != -1:
+                numbers.append((index, num.index(n)))
+                index = line.find(n, index + 1)
         for (i, c) in enumerate(line):
             try:
                 number = int(c)
-                numbers.append(number)
+                numbers.append((i, number))
             except:
                 continue
-        count += numbers[0]*10 + numbers[-1]
+        numbers = sorted(numbers, key=lambda x: x[0])
+        count += numbers[0][1]*10 + numbers[-1][1]
 
     print("The number is: ", count)
--- README.md
+++ README.md
@@ -5,7 +5,15 @@ This is the repository of my work on the Advent of Code 2023 code
 
 # Puzzle 1
 
-    The idea is to look for each caracter of the line and check if it's a number. The option I went with is to add all
-    result inside an array then add the first number find time ten and the last.
+    The idea is to look for each caracter of the line and check if it's a number. The option I went with is to
+    add all result inside an array then add the first number find time ten and the last.
+
+    The code works and I got the star.
+
+# Puzzle 2
+
+    The idea is the same as puzzle 1 but now to also check for instance of number in text. The option I went with
+    is to check first for all instances, add them with their index, check for number the sort the array by the
+    index and do the same operation.
 
     The code works and I got the star.
